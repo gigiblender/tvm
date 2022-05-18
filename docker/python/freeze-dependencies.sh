8155300f8f7a63496f1bb688f571060952fd6115
Andrew Reusch <areusch@gmail.com>
2022-05-18 11:51:51 -0700

    fix freeze-deps computation of docker image name

--- docker/python/freeze-dependencies.sh
+++ docker/python/freeze-dependencies.sh
@@ -6,7 +6,7 @@ set -eux
 cd "$(dirname "$0")/../.."
 
 # NOTE: working dir inside docker is repo root.
-BUILD_TAG=$(echo "${BUILD_TAG:-tvm}" | python3 -c 'import sys; import urllib.parse; print(urllib.parse.quote(sys.stdin.read(), safe=\"\").lower())' | tr % -)
+BUILD_TAG=$(echo -n "${BUILD_TAG:-tvm}" | python3 -c 'import sys; import urllib.parse; print(urllib.parse.quote(sys.stdin.read(), safe="").lower())' | tr % -)
 docker/bash.sh -i "${BUILD_TAG}.ci_py_deps:latest" python3 docker/python/freeze_deps.py \
                --ci-constraints=docker/python/ci-constraints.txt \
                --gen-requirements-py=python/gen_requirements.py \
