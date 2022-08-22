0b5adaa23c5b2af74981b9487e5f36b60ec4d542
Florin Blanaru <florin.blanaru96@gmail.com>
2022-08-22 19:05:31 +0300

    fix

--- tests/scripts/github_skipped_tests_comment.py
+++ tests/scripts/github_skipped_tests_comment.py
@@ -203,7 +203,7 @@ if __name__ == "__main__":
 
         if comment is not None:
             comment_url = comment["url"]
-            comment_id = comment_url[comment_url.find("comments/"): len(comment_url)]
+            comment_id = comment_url[comment_url.find("comments/"): len(comment_url)].strip("comments/")
             github.patch(f'issues/comments/{comment_id}', {"body": body})
         else:
             github.post(url, {"body": body})
